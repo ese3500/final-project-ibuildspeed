ab4545bb4f591756e2530887608f6f1e2c974b0e
aabdel0181 <68669398+aabdel0181@users.noreply.github.com>
2024-04-30 21:11:26 -0400

    Delete iBuildSpeed directory

--- iBuildSpeed/iBuildSpeed/adc.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADC_H
-#define ADC_H
-#include <avr/interrupt.h>
-#include <avr/io.h>
-#include <stdio.h>
-#include <stdlib.h>
-uint16_t readADC(uint8_t ADCchannel);
-void Initialize_ADC();
-#endif
